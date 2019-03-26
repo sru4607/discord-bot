08f6a940968d328a89eb6ace9af14fab6d5cd25e
sunferth <srunferth@carolina.rr.com>
2019-03-25 23:49:24 -0400

    Added Discord.js (unused) Edited Kilroy Added in Author command Added in changing Nickname Fixed Error in include code
    Signed-off-by: sunferth <srunferth@carolina.rr.com>

--- bot.js
+++ bot.js
@@ -1,8 +1,18 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+//Magic API
 var ScryfallClient = require('scryfall-client')
 var scryfall = new ScryfallClient()
+//Read in author ASCII art
+var fs = require('fs')
+var secret;
+fs.readFile('Kilroy.txt', (err, data) => { 
+    if (err) throw err; 
+    secret = data.toString();
+	console.log(secret)
+}) 
+ 
 var callback = function(){
 	
 }
@@ -26,7 +36,11 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `$`
+	//Server ID
+	var SID = bot.channels[channelID].guild_id;
+	//Prefix
     if (message.substring(0, 1) == '$') {
+		//Search
 		if(message.substring(1,2) == 'S' || message.substring(1,2) == 's')
 		{
 			var args = message.substring(8);
@@ -53,6 +67,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					});
 			})
 		}
+		//Image
 		else if(message.substring(1,2) == 'I' || message.substring(1,2) == 'i')
 		{
 			var args = message.substring(7);
@@ -71,6 +86,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					});
 			})
 		}
+		//repeat after me
 		else if(message.substring(1,15).toLowerCase() == "magic bot says")
 		{
 			
@@ -79,6 +95,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 						message: message.substring(16)
 					});
 		}
+		//Not a command
 		else{
 
 			bot.sendMessage({
@@ -89,9 +106,32 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 	
 		
 	}
-
-//testing includes()
-	if (message.includes("magic bot"){
-		bot.sendMessage({to:channelID, message: "owo"});
+	//testing includes()
+	if (message.toLowerCase().includes("magic bot")){
+		bot.sendMessage({
+			to:channelID, 
+			message: "owo"
+			});
 	}
-};
+	//Author Message
+	if (message.toLowerCase().includes("magic bot author")){
+		bot.sendMessage({
+			to:channelID, 
+			message: "```" + secret + "```"
+			});
+	}
+	
+	//change bot nickname
+	if(message.toLowerCase().includes('nick')){
+		console.log(userID);
+		console.log(SID);
+		bot.editNickname({
+			serverID: SID,
+			userID: bot.id, 
+			nick: message.replace('nick ', '')
+		},function(err){
+			console.log(err)
+		});
+	}
+
+});
